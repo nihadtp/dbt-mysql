d76e334822f41cf1f481eb29634d7aae593a84e4
nihadtp <nihadtp93.mec@gmail.com>
2024-02-17 12:43:37 -0500

    Added staging model for wemployees

--- mysql_employee/models/example/stg_employee.sql
+++ mysql_employee/models/example/stg_employee.sql
@@ -7,19 +7,10 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
+SELECT E.*, D.* FROM 
+employees.employees E join 
+employees.dept_emp DE on E.emp_no = DE.emp_no 
+JOIN employees.departments D on DE.dept_no = D.dept_no
 /*
     Uncomment the line below to remove records with null `id` values
 */
